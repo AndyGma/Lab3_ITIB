fbe3e3a2116e253916d632c384ecfe23b71e1195
Andrew Klokov <555135@mail.ru>
2021-10-07 00:01:10 +0300

    Init

--- test.py
+++ test.py
@@ -3,18 +3,17 @@ class Cat:
     age = None
     isHappy = None
 
-    def set_data(self, name, age, isHappy):
+    def __init__(self, name, age, isHappy):
+        self.set_data(name, age, isHappy)
+        self.get_data()
+
+    def set_data(self, name = None, age = None, isHappy = None):
         self.name = name
         self.age = age
         self.isHappy = isHappy
+
     def get_data(self):
         print(self.name, "age = ", self.age, ". Happy : ", self.isHappy)
 
-cat1 = Cat()
-cat1.set_data("Барсик", 3, True)
-
-cat2 = Cat()
-cat2.set_data("Жопик", 2, False)
-
-cat1.get_data()
-cat2.get_data()
+cat1 = Cat("Барсик", 3, True)
+cat2 = Cat("Жопик", 2, False)
